2fa1e02a706393127e2972944cc4545491e41b2c
AnnaAndrlova <andrlovaanicka@seznam.cz>
2023-10-27 22:06:21 +0200

    API for projects is done

--- src/main/java/rest/controllers/ApiProjectController.java
+++ src/main/java/rest/controllers/ApiProjectController.java
@@ -1,5 +1,6 @@
 package rest.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import rest.models.Project;
@@ -7,13 +8,20 @@ import rest.repo.ProjectRepo;
 
 import java.util.List;
 
+@RestController
 public class ApiProjectController {
 
+    @Autowired
     private ProjectRepo projectRepo;
+
 
     @GetMapping(value = "/projects")
     public List<Project> getProjects() {
         return projectRepo.findAll();
+    }
+    @GetMapping(value = "/project/{id}")
+    public Project getProjects(@PathVariable long id) {
+        return projectRepo.findById(id).get();
     }
 
     @PostMapping(value = "/saveProject")
@@ -23,7 +31,19 @@ public class ApiProjectController {
     @PutMapping(value = "/updateProject/{id}")
     public void updateProject(@PathVariable long id, @RequestBody Project project){
         Project updatedProject = projectRepo.findById(id).get();
+        updatedProject.setDeadline(project.getDeadline());
+        updatedProject.setDescription(project.getDescription());
+        updatedProject.setProjectName(project.getProjectName());
+        updatedProject.setStatus(project.getStatus());
+        updatedProject.setSubject(project.getSubject());
     }
+    @DeleteMapping (value = "/delete/{id}")
+    public void deleteProject(@PathVariable long id){
+        Project deleteProject = projectRepo.findById(id).get();
+        projectRepo.delete(deleteProject);
+
+    }
+
 
 }
 
